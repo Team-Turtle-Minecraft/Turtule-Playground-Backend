db7efa600c6c00ab5e86586fd017b93e1bc5e3ed
Jeong Seonghun <2000tjdgns@naver.com>
2024-12-24 18:47:09 +0900

    init:프로젝트 생성

---
+++ src/main/java/org/turtle/minecraft_service/MinecraftServiceApplication.java
@@ -0,0 +1,13 @@
+package org.turtle.minecraft_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinecraftServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/turtle/minecraft_service/MinecraftServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.turtle.minecraft_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
