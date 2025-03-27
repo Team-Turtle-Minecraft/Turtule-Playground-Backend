3a5a003d9f28b8f971c559e51dfd9f8974aa79a2
Jeong Seonghun <2000tjdgns@naver.com>
2025-03-27 17:24:53 +0900

    feat:유저 수 가져오는 출처 레포지토리 변

--- src/main/java/org/turtle/minecraft_service/repository/primary/UserRepository.java
+++ src/main/java/org/turtle/minecraft_service/repository/primary/UserRepository.java
@@ -1,6 +1,8 @@
 package org.turtle.minecraft_service.repository.primary;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.turtle.minecraft_service.domain.primary.user.User;
 
 import java.util.List;
@@ -10,4 +12,7 @@ public interface UserRepository  extends JpaRepository<User, Long> {
     Optional<User> findBySnsId(String snsId);
     Optional<User> findByNickname(String nickname);
     List<User> findAllByNicknameIn(List<String> nicknames);
+
+    @Query("SELECT COUNT(u) FROM users u WHERE u.nickname NOT IN (:gmPlayerNames)")
+    int findAllUsersWithoutGM(@Param("gmPlayerNames") List<String> gmPlayerNames);
 }
